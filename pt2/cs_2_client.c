7133d4aae7665e53fbbec85620446c5bd64ea157
MagikarpUsedFly <wr140815@ohio.edu>
2019-04-16 01:13:54 -0400

    final pt2 finishied

---
+++ pt2/cs_2_client.c
@@ -0,0 +1,244 @@
+//******************************************************************************
+// File:        cs_2_client.c
+// Author:      Wesley Ryder
+// Date:        4-3-19
+// Description: To use pipes and forks to create a server/client application
+//              using pipes and forks this is the client implementation
+//              *note* references were made on general info about pipes from
+//              online and how they work these were online forums and general
+//              documentation etc
+//******************************************************************************
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#define MSG_LENGTH 100  // maximum length of message
+#define READ 0          // Read Value
+#define WRITE 1         // Write Value
+#define MAX_BUF 1024    // Max Buffer
+
+
+// Type Enum as described in lecture
+typedef enum {
+  SEND,
+  EXIT,
+  STATUS,
+  TIME,
+  RETURN_STRING,
+  RETURN_TIME
+} cmd_type_t;
+
+// Enum struct from lecture
+typedef enum {
+  REGULAR,
+  COMMAND
+} msg_type_t;
+
+// Init Struct
+typedef struct msg_init {
+  int pid_num;
+} pid_sent_t;
+
+// Child Struct
+typedef struct msg_request {
+  msg_type_t type;                // Command
+  cmd_type_t subtype;             // Time or Status
+  int status;                     // status
+  int PID;                        // pid
+  char message_text[MSG_LENGTH];  // mssg
+} cargo_t;
+
+//******************************************************************************
+// Function:    main()
+// Parameters:  None
+// Description: To run the main of the server side
+//******************************************************************************
+int main()
+{
+  // Variable Declarations Needed
+  char buf[MAX_BUF];
+  char bufOne[MAX_BUF];
+  char bufTwo[MAX_BUF];
+  int fd;
+  int fSend;
+  int fRec;
+
+  // Setting up fifo dir and write to server
+  char *server_fifo = "/tmp/server_np";
+  fd = open(server_fifo, O_WRONLY);
+
+	// Send Init
+	pid_sent_t _init;
+  // Set PID
+	_init.pid_num = getpid();
+
+  // Write _init but if fail dispaly init failure
+	if( write(fd, &_init, sizeof(pid_sent_t)) < 0)
+  {
+    fprintf(stderr, "Error: Init failure\n");
+  }
+  close(fd);
+
+	// Setting up pipes
+	sprintf(bufOne, "/tmp/%s_send", buf);
+	char *fifoSend = bufOne;
+	mkfifo(fifoSend, 0666);
+
+	sprintf(bufTwo, "/tmp/%s_receive", buf);
+	char *fifoRec = bufTwo;
+	mkfifo(fifoRec, 0666);
+
+	//read input from terminal for message requests
+	while(1)
+	{
+		//open pipe and declare variables
+		fSend = open(fifoSend, O_WRONLY);
+    // Set pointer to NULL
+		char *cmd = NULL;
+    // Set buffer_size = to max buff
+		size_t buffer_size = MAX_BUF;
+    // Declare str
+    char str[MSG_LENGTH];
+    // Declare message one
+		struct msg_request msgOne;
+
+    // Display to user the command options
+    printf("\nEnter a command:\n1) send: followed by a message\n2) exit\n3) status\n4) time\n");
+    // Get input from user
+		scanf("%s", str);
+
+		// This is for the send command case
+		if ( strstr(str, "Send:" ) != NULL || strstr( str, "send:" ) != NULL )
+		{
+			// Get the inputed Send: command string
+			size_t characters = getline(&cmd, &buffer_size, stdin);
+      // Removing the newline and replacing with null
+			cmd[ strlen(cmd) - 1 ] = '\0';
+			memmove(str, str + 1, strlen(str));
+			// Set corrosposnding type and subtype
+      msgOne.subtype = SEND;
+			msgOne.type = COMMAND;
+      msgOne.PID = getpid();
+
+      // Copy the message txt from the msg
+			strcpy(msgOne.message_text, cmd);
+			// Now we write to server or print error if failure
+		  if(write(fSend, &msgOne, sizeof(cargo_t)) < 0)
+			{
+		    fprintf(stderr, "Error: Write to server failure\n");
+				close(fSend);
+			}
+      // Succesfull
+			else{close(fSend);}
+		}
+    // EXIT COMMAND
+    else if (strstr(str, "Exit") != NULL || strstr(str, "exit") != NULL)
+    {
+      // Setting corrosponding struct values
+      msgOne.subtype = EXIT;
+      msgOne.type = COMMAND;
+
+      // Copy the proceding text from the mssg
+      strcpy(msgOne.message_text, str);
+
+      if(write(fSend, &msgOne, sizeof(cargo_t)) < 0)
+      {
+        fprintf(stderr, "Error: Write to server error my guy\n");
+        close(fSend);
+      }
+      // Successfull so close pipeeeeee
+      else
+      {
+        close(fSend);
+      }
+
+      exit(0);
+    }
+    //TIME COMMAND
+    else if (strstr(str, "Time") != NULL || strstr(str, "time") != NULL)
+    {
+      // Set corrsponding type and suptype
+      msgOne.subtype = TIME;
+      msgOne.type = COMMAND;
+
+      // Copy the string from  message
+      strcpy(msgOne.message_text, str);
+
+      //send struct to server
+      if(write(fSend, &msgOne, sizeof(cargo_t)) < 0)
+      {
+        fprintf(stderr, "Error: Write error my dude\n");
+        close(fSend);
+      }
+      else
+      {
+        close(fSend);
+        //open pipe to get struct
+        fRec = open(fifoRec, O_RDONLY);
+
+        // Declation of cargo
+        cargo_t ret_time;
+
+        //read from server
+        while(read(fRec, &ret_time, sizeof(cargo_t))>0)
+        {
+          if(ret_time.subtype == RETURN_TIME)
+          {
+            printf("%s\n", ret_time.message_text);
+          }
+          close(fRec);
+        }
+      }
+    }
+		// Status input case
+		else if (strstr(str, "Status") != NULL || strstr(str, "status") != NULL)
+		{
+			// Set corrosponding type and sutype
+      msgOne.subtype = STATUS;
+			msgOne.type = COMMAND;
+
+      // Copy the message txt from the msg
+			strcpy(msgOne.message_text, str);
+			//write to server
+			if(write(fSend, &msgOne, sizeof(cargo_t)) < 0)
+			{
+		    fprintf(stderr, "Error: Write to server error my dude\n");
+				close(fSend);
+			}
+			//get status back from server after written
+			else
+			{
+				// Close pipe because we were succesfull
+				close(fSend);
+				// Open frec
+				fRec = open(fifoRec, O_RDONLY);
+
+        // Declare Return_status
+        cargo_t return_stat;
+
+        // Retrieve back from server with value corrosposnding to status value
+				while(read(fRec, &return_stat, sizeof(cargo_t)) > 0)
+				{
+          // Display to user the status
+					if(return_stat.subtype == RETURN_STRING)
+					{
+						printf("%s%d\n", return_stat.message_text, return_stat.status);
+					}
+          // close pipe
+					close(fRec);
+				}
+			}
+		}
+    // This is the catch all for bad imputs
+		else{printf("%s%s", str, "Input was not a valid command...\n");}
+	}
+    return 0;
+}
---
+++ pt2/cs_2_server.c
@@ -0,0 +1,265 @@
+//******************************************************************************
+// File:        cs_2_server.c
+// Author:      Wesley Ryder
+// Date:        4-3-19
+// Description: To use pipes and forks to create a server/client application
+//              using pipes and forks this is the server implementation
+//              *note* references were made on general info about pipes from
+//              online and how they work these were online forums and general
+//              documentation etc
+//******************************************************************************
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+
+int processes = 0;      // Child processess
+#define MSG_LENGTH 100  // maximum length of message
+#define READ 0          // Read Value
+#define WRITE 1         // Write Value
+#define MAX_BUF 1024    // Max buffer
+
+
+// Type Enum as described in lecture
+typedef enum {
+  SEND,
+  EXIT,
+  STATUS,
+  TIME,
+  RETURN_STRING,
+  RETURN_TIME
+} cmd_type_t;
+
+// Enum struct from lecture
+typedef enum {
+  REGULAR,
+  COMMAND
+} msg_type_t;
+
+// Init Struct
+typedef struct msg_init {
+  int pid_num;
+} pid_sent_t;
+
+// Child Struct
+typedef struct msg_request {
+  msg_type_t type;                // Command
+  cmd_type_t subtype;             // Time or status
+  int status;                     // status
+  int PID;                        // PID;
+  char message_text[MSG_LENGTH];  // msg
+} cargo_t;
+
+//******************************************************************************
+// Function:    unLink()
+// Parameters:  int s
+// Description: To unlink when user enters a control c command so we have clean
+//              exit for server
+//******************************************************************************
+void unLink(int s)
+{
+  char *server_fifo = "tmp/server_np";
+  unlink(server_fifo);
+  exit(0);
+}
+
+//******************************************************************************
+// Function:    handleD
+// Parameters:  int s
+// Description: If ctr+c is entered handle
+//******************************************************************************
+void handleD(int s)
+{
+	pid_t pid;
+	pid = wait(NULL);
+}
+
+
+//******************************************************************************
+// Function:    main()
+// Parameters:  None
+// Description: To run the main of the server side
+//******************************************************************************
+int main()
+{
+  //initialization of pipe and buffers
+  char *server_fifo = "/tmp/server_np";
+  char buf[MAX_BUF];
+	char PID_sent[MAX_BUF];   //Pid sent char array
+  int ret_f;                // ret_f
+  int fd;                   //File Dir
+  int fSend;                //fSend
+  int fRec;                 //fRec
+
+  // implemented for Ctr C case so we have clean exit
+  signal(SIGCHLD, handleD);
+  signal(SIGINT, unLink);
+
+	int server_pid = getppid();
+  mkfifo(server_fifo, 0666);
+
+  // While true and pid == server pid run loop iniftitly
+  while(1 && getppid() == server_pid)
+  {
+	  // check for clients sending info
+    fd = open(server_fifo, O_RDONLY);
+		pid_sent_t _init;
+
+		 //read pid sent by client
+    while(read(fd, &_init, sizeof(pid_sent_t)) > 0)
+    {
+      processes++;
+      printf("Recieved: %i\n", _init.pid_num);
+			sprintf(PID_sent, "%i", _init.pid_num);
+			ret_f = fork();
+    }
+
+    // If the for is the child then do the proceeding block
+		if(ret_f == 0)
+		{
+      // Declare Buffer arrays for server
+      char bufOne[MAX_BUF];
+      char bufTwo[MAX_BUF];
+
+
+			sprintf(bufOne, "/tmp/%s_send", buf);
+			char *fifoSend = bufOne;
+
+			sprintf(bufTwo, "/tmp/%s_receive", buf);
+			char *fifoRec = bufTwo;
+
+        // infinite loop
+		    while(1)
+		    {
+          // Declaring mesgOne
+          struct msg_request msgOne;
+
+          // check for client sending
+          fSend = open(fifoSend, O_RDONLY);
+
+				//read struct sent by client
+		    while(read(fSend, &msgOne, sizeof(cargo_t)) > 0)
+        {
+
+          // Commands same idea as last time in terms of matching the only difference
+          // now being that we have to keep opening and closing pipes while also
+          // giving the structs corrpsonding values for the structs variables
+          if (msgOne.type == COMMAND)
+          {
+            // SEND case
+            // Displaying the pid from server and client and the msg passed and reiveid
+            // along with their corrospoinding pids
+            if (msgOne.subtype == SEND)
+            {
+              printf("server (PID: %d) Recieved string %s from client (PID: %d)\n",
+                        getpid(),
+                        msgOne.message_text,
+                        msgOne.PID);
+            }
+            // EXIT case
+            // This is the same as part one implementation idea
+            else if (msgOne.subtype == EXIT)
+            {
+              // deincremnt the count of cleints open
+              // and then rpint the reiveid msg and the pid from the client that
+              // is being closed
+              processes--;
+              printf("Recieved: %s from client (PID: %d), client now exiting...\n",
+                    msgOne.message_text,
+                    msgOne.PID);
+              _exit(0);
+
+            }
+            // STATUS CASE
+            else if(msgOne.subtype == STATUS)
+						{
+              // close pipe
+							close(fSend);
+		          // Open
+							fRec = open(fifoRec, O_WRONLY);
+
+              // Declaring struct return_stat
+							cargo_t return_stat;
+
+              // Setting the corropsonding values of status and subtype
+              return_stat.status = processes;
+							return_stat.subtype = RETURN_STRING;
+
+              // Coping the mssg to display to user the content
+						  strcpy(return_stat.message_text, "Number of processes currently running: ");
+
+              // Now we write back to client and displaying error if fail
+						  if( write(fRec, &return_stat, sizeof(cargo_t)) < 0)
+              {
+                fprintf(stderr, "Error: write to client error\n");
+              }
+							else
+              {
+                close(fRec);
+              }
+						}
+						// Time Case
+						else if (msgOne.subtype == TIME)
+						{
+              // close
+							close(fSend);
+
+              // General getting time application found on standard
+              // forum post. Pretty common found on StackOVerflow.
+              // get_time_of day guide
+              /////////////////////////////////////////////////////////////////////////
+							time_t tmp_time;
+							struct tm *tmp_local_time;
+							tmp_time = time (NULL);
+							tmp_local_time = localtime (&tmp_time);
+              /////////////////////////////////////////////////////////////////////////
+
+              // Opening pipe
+							fRec = open(fifoRec, O_WRONLY);
+
+              // Declare the time to be set for value return
+							cargo_t time_val;
+              // Setting time for that said struct
+							time_val.subtype = RETURN_TIME;
+
+              // General formating for displaying time to user
+              strftime (time_val.message_text, 150, "Current time: [%I:%M] %p.",
+                          tmp_local_time);
+
+              // Write to client, if error display error
+						  if( write(fRec, &time_val, sizeof(cargo_t)) < 0)
+              {
+                fprintf(stderr, "Error: Write to client error my dude\n");
+              }
+							else
+              {
+                close(fRec);
+              }
+						}
+					}
+
+        }
+        close(fSend);
+      }
+			return(0);
+    }
+    // Else we continue and the fork of the parent will return
+    else
+    {
+      continue;
+    }
+      // close that pipeee
+      close(fd);
+  }
+  unlink(server_fifo);
+  return 0;
+}
